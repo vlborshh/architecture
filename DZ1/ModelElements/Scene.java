1852cfd2d5a7e03a2690722be1e3fab76d812d97
vlborshh <vlborshh@yandex.ru>
2023-08-15 11:28:22 +0300

    исправил

--- DZ1/ModelElements/Scene.java
+++ DZ1/ModelElements/Scene.java
@@ -3,9 +3,23 @@ package DZ1.ModelElements;
 import java.util.List;
 
 public class Scene {
-    public int id;
+    public int Id;
     public List<PoligonalModel> Models;
     public List<Flash> Flashes;
+    public List<Camera> Cameras;
+
+    public Scene(int id, List<PoligonalModel> model, List<Flash> flashes, List<Camera> cameras) throws Exception {
+
+        Id = id;
+        if (model.size() > 0) {
+            Models = model;
+        } else {
+            throw new Exception("Должна передаваться как минимум одна модель");
+        }
+        Flashes = flashes;
+        Cameras = cameras;
+
+    }
 
     public Object method1(Object i) {
         return i;
