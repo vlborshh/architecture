8e539750a4cc868850f4cedab6fc1a92a76f3ee8
vlborshh <vlborshh@yandex.ru>
2023-08-28 12:12:19 +0300

    Что успел - работы много

--- DZ4/Main.java
+++ DZ4/Main.java
@@ -1,16 +1,11 @@
 package DZ4;
 
-import java.time.LocalDate;
-
-import DZ4.Services.Customer;
-import DZ4.Services.Ticket;
+import DZ4.Model.Bus;
 
 public class Main {
     public static void main(String[] args) {
-        Customer customer = new Customer(1);
-        Ticket ticket = new Ticket(1, 2598.70, -1, LocalDate.now());
-        System.out
-                .println("Покупатель с ID: " + customer.getId() + " купил билет под номером: " + ticket.getRootNumber()
-                        + " по цене: " + ticket.getPrice() + " от: " + ticket.getDate());
+        Bus bus = new Bus();
+        bus.buyingBusTicket();
+        System.out.println(bus);
     }
 }
